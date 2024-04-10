91fc52622fc3c982124b2c4ba17aeef0c75d557f
Navaphat <phiboon_n@silpakorn.edu>
2024-04-10 19:54:46 +0700

    change callback

--- scripts/robot_controller.py
+++ scripts/robot_controller.py
@@ -5,6 +5,7 @@ import numpy as np
 from sensor_msgs.msg import Image
 import math
 from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Pose
 
 class bot_control:
     def __init__(self) :
@@ -14,19 +15,40 @@ class bot_control:
         self.velocity_msg.angular.x = 0
         self.velocity_msg.angular.y = 0
         self.pub = rospy.Publisher('/spot/cmd_vel' , Twist , queue_size = 10)
-        self.state = rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
-        self.P = 0.004 #rospy.get_param("line_follower_controller/pid/p")
+        self.P = 0.004
 
-    
-      #self.image_sub = Line_Follower()
+        self.robot_names = "spot"
+        self.target_points = [Pose()]
 
-    def callback(self, msg):
-        index = msg.name.index("spot")
-        x = msg.pose[index].position.x
-        y = msg.pose[index].position.y
-        z = msg.pose[index].position.z
-        if (x >= 1.965639 and x <= 1.972989 and y <= 1.972989 and y >= -3.229230):
-            self.stop()
+        # self.target_point.position.x = 0.202972
+        # self.target_point.position.y = 6.643107
+
+        self.target_points[0].position.x = 1.761787
+        self.target_points[0].position.y = -2.607944
+
+        self.model_state_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self.callback)
+
+    # def callback(self, data):
+    #     for i, robot_name in enumerate(self.robot_names):
+    #         for j, target_point in enumerate(self.target_points):
+    #             if not self.reached_targets[j]:
+    #                 try:
+    #                     index = data.name.index(robot_name)
+    #                     robot_pose = data.pose[index]
+    #                     distance_to_target = math.sqrt((robot_pose.position.x - target_point.position.x)**2 + (robot_pose.position.y - target_point.position.y)**2)
+    #                     if distance_to_target < 0.5:
+    #                         self.stop()
+    #                 except ValueError:
+    #                     pass
+
+    def callback(self, data):
+        index = data.name.index(self.robot_names)
+        robot_pose = data.pose[index]
+
+        for i, target_point in enumerate(self.target_points):
+            distance_to_target = math.sqrt((robot_pose.position.x - target_point.position.x)**2 + (robot_pose.position.y - target_point.position.y)**2)
+            if distance_to_target < 0.5:
+                self.stop()
 
     #move function to move robot
     def move(self,linear,angular):
@@ -63,6 +85,7 @@ class bot_control:
          self.move(0, -0.6)
     
     def stop(self):
+         rospy.loginfo('reach the Goal.')
          rospy.signal_shutdown('Reach the Goal.')
 
     def turn_left(self):
